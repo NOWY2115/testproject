f0647a85d196b5c3d176612266d9a8087489db94
NOWY <nowyyy2115@gmail.com>
2024-12-09 20:33:52 +0100

    a little bit more content, mobile functionality etc.

--- js/main.js
+++ js/main.js
@@ -2,11 +2,13 @@ window.onscroll = () => {
 	changeMenu()
 }
 
-let menu = document.querySelector(".menu-container")
-let detectSticky = menu.offsetTop
+
+
 
 let changeMenu = () => {
-	if(window.pageYOffset > detectSticky) 
+	let menu = document.querySelector(".menu-container")
+	let detectSticky = menu.offsetTop
+	if (window.scrollY > detectSticky)
 		menu.classList.add("sticky-menu")
 	else menu.classList.remove("sticky-menu")
 }
