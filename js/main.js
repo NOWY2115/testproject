0cd1c27267d12ace41851cfc3a106b730305858f
NOWY <nowyyy2115@gmail.com>
2025-02-02 17:21:00 +0100

    Finishing the design, adding the contact form, footer, preparations for moving to backend-supportive infrastructure

--- js/main.js
+++ js/main.js
@@ -2,8 +2,14 @@ window.onscroll = () => {
 	changeMenu()
 }
 
+let checkLength = (element, index) => {
+	if (element.value != '' && !labels[index].classList.contains('not-empty')) {
+		labels[index].classList.add('not-empty')
+	}
 
-
+	if (element.value == '')
+		labels[index].classList.remove('not-empty');
+}
 
 let changeMenu = () => {
 	let menu = document.querySelector(".menu-container")
@@ -11,4 +17,21 @@ let changeMenu = () => {
 	if (window.scrollY > detectSticky)
 		menu.classList.add("sticky-menu")
 	else menu.classList.remove("sticky-menu")
+}
+
+let inputs = document.querySelectorAll('.contact-form input[type="text"], .contact-form input[type="email"]')
+let textareas = document.querySelectorAll('.contact-form textarea')
+let fields = [];
+for (let i = 0; i < inputs.length; i++) {
+	fields.push(inputs[i])
+}
+for (let i = 0; i < textareas.length; i++) {
+	fields.push(textareas[i])
+}
+let labels = document.querySelectorAll('.contact-form .input-label')
+
+for (let i = 0; i < fields.length; i++) {
+	fields[i].addEventListener('input', () => {
+		checkLength(fields[i], i)
+	})
 }
